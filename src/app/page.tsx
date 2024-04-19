57af02fd989410bf3fd49f065992c86b9e8ebda5
Jack White <jackwhite230@gmail.com>
2024-04-19 09:39:31 +0100

    code screenshots added. links added. learning content filled.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,9 +29,8 @@ export default function Home() {
           </li>
           <li>
             Understand the basic building blocks of JavaScript: variables, data
-            types, functions.
+            types, objects and arrays.
           </li>
-          <li>Be comfortable working with objects and arrays in JavaScript.</li>
           <li>
             Grasp the benefits of using TypeScript as a superset of JavaScript
             code.
@@ -121,7 +120,155 @@ export default function Home() {
             efficiency.
           </li>
         </ul>
-        <h2></h2>
+
+        <h2>Variables</h2>
+        <p>
+          Now to actually use some data in JavaScript. Say we want to write a
+          message, this will of course be a <b>string</b>.<br />
+          We can do this with either <b>let</b> or <b>const</b>. Do some
+          research to find out the difference!
+        </p>
+        <Image
+          src="/js-example-1.png"
+          alt="code example"
+          height="60"
+          width="370"
+        />
+        <p>
+          JavaScript understands that there is a string here. But it will not
+          throw an error if that data is redefined later on until we try and run
+          the code. It would be better if we could check this as we are writing
+          right? <br />
+          <br />
+          Enter TypeScript, where we can now define the type and it will do our
+          <b> static type checking</b> for us. The way we define this is as
+          below:
+        </p>
+        <Image
+          src="/ts-example-1.png"
+          alt="code example"
+          height="70"
+          width="400"
+        />
+        <p>
+          We can use this way of defining types for all{" "}
+          <b>primitive data types</b>:
+        </p>
+        <Image
+          src="/example-2.png"
+          alt="code example"
+          height="200"
+          width="350"
+        />
+
+        <h2>Objects and Arrays</h2>
+        <h3>Objects</h3>
+        <p>
+          In JavaScript, <b>objects</b> are a collection of key-value pairs.
+          This means that we have one object with multiple parameters to it. The
+          way that we write these is best shown in an example.
+        </p>
+        <Image
+          src="/js-example-3.png"
+          alt="code example"
+          height="130"
+          width="230"
+        />
+        <p>
+          Again, we need to look at how TypeScript will interpret this. This
+          time we need to use <b>interface</b> to define the object at all
+          levels, as it can contain any data type within it.{" "}
+        </p>
+        <Image
+          src="/ts-example-3.png"
+          alt="code example"
+          height="250"
+          width="280"
+        />
+
+        <h3>Arrays</h3>
+        <p>
+          An array is an ordered collection of things. It is ordered by the
+          index, taking a numerical position in the array starting from the
+          first item at index 0. Items in an array can be of any data type. To
+          write an array in JavaScript, you would do it like this:
+        </p>
+        <Image
+          src="/js-example-4.png"
+          alt="code example"
+          height="60"
+          width="280"
+        />
+        <p>
+          In JavaScript, we need to define what types are going to be in the
+          array. They can be the same, as in the first example, or different,
+          like in the second.
+        </p>
+        <Image
+          src="/ts-example-4.png"
+          alt="code example"
+          height="80"
+          width="420"
+        />
+
+        <h3>Your Task</h3>
+        <p>
+          Time to put this into practice. Follow this{" "}
+          <a href="https://www.typescriptlang.org/play">link</a> to go to an
+          interactive sandbox where you can test out what you have learned. Code
+          some basic TypeScript to satisfy the following:
+        </p>
+        <ul>
+          <li>
+            A <b>variable</b> of name, where the value is your name.
+          </li>
+          <li>
+            A <b>variable</b> detailing the number of keys on your keyboard.
+          </li>
+          <li>
+            A <b>variable</b> of leftHanded, where the value is true or false.
+          </li>
+          <li>
+            An <b>object</b> detailing your favourite animal. Include values for
+            colour, where it lives, and a boolean for if it is an endangered
+            species or not.
+          </li>
+          <li>
+            An <b>array</b> which has the items of your last shopping trip.
+          </li>
+        </ul>
+
+        <h2>Hungry for more?</h2>
+        <p>
+          Take a look at these resources for some of the best learning around
+          the internet:
+        </p>
+        <ul>
+          <li>
+            Test your coding in this sandbox environment!{" "}
+            <a href="https://www.typescriptlang.org/play">
+              www.typescriptlang.org/play
+            </a>
+          </li>
+          <li>
+            It is always good to start with the official docs:{" "}
+            <a href="https://www.typescriptlang.org/docs/">
+              www.typescriptlang.org/docs/
+            </a>
+          </li>
+          <li>
+            One of the best resources for learning new content:{" "}
+            <a href="https://www.freecodecamp.org/news/learn-typescript-beginners-guide/">
+              www.freecodecamp.org/news/learn-typescript-beginners-guide/
+            </a>
+          </li>
+          <li>
+            Do you prefer learning by video tutorial?{" "}
+            <a href="https://www.youtube.com/watch?v=gieEQFIfgYc&ab_channel=DaveGray">
+              www.youtube.com/watch?v=gieEQFIfgYc&ab_channel=DaveGray
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
   );
