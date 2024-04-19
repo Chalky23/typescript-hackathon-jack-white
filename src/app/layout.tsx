7cb897cbc6ac52421df939b7991c4044e1718a4e
Jack White <jackwhite230@gmail.com>
2024-04-19 08:19:13 +0100

    favicon and further content added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/logos.jpg" sizes="any" />
+      </head>
       <body className={montserrat.className}>{children}</body>
     </html>
   );
